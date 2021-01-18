1aa69b8bfe830779500031019c87596077c0583b
abdel <aboualyet63@gmail.com>
2021-01-18 11:10:57 -0500

    created some functionalities

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React,{Component } from 'react';
+import Particles from 'react-particles-js';
 import './App.css';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo'
 import ImageLink from './Components/ImageLink/ImageLink';
 import Rank from './Components/Rank/Rank'
 
+const particleOption = {
+    particles: {
+      numbers: { value:100,
+        density: {
+          enable: true,
+          value_area:800
+       
+        }
+      }
+    }
+  }
+  
+
 
 
 class App extends Component {
+
+  
+
   render(){
 
     return (
       <div className="App">
-        <header className="App-header">
-         <Navigation/>
-         <Logo/>  
-         <Rank/>
-         <ImageLink/>
-         
-        </header>
-      </div>
+          <Particles className='particles' params={particleOption}/>
+          <Navigation/>
+          <Logo/>  
+          <Rank/>
+          <ImageLink/>
+        </div>
     );
     
   }
